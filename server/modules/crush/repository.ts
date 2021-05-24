aa2f26b5569e2904135c348a172109ab6349b815
Leonardo Oliveira <62403037+srgoogle23@users.noreply.github.com>
2021-05-24 19:46:09 -0300

    cleanup code

--- server/modules/crush/repository.ts
+++
@@ -1,4 +0,0 @@
-import * as mongoose from 'mongoose';
-import CrushSchema from './schema';
-
-export default mongoose.model('Crush', CrushSchema);
--- server/server.ts
+++
@@ -1,5 +0,0 @@
-import App from './app';
-App.app.listen(process.env.PORT, () => console.log('servidor está rodando na porta: ' + process.env.PORT));
-
-process.once('SIGUSR2', () => App.closeDataBaseConnection('nodemon restart', ()=> process.kill(process.pid, 'SIGUSR2')));
-process.on('SIGINT', () => App.closeDataBaseConnection('execução foi interrompida', ()=> process.exit(0)));
