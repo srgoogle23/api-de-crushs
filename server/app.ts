7a1cfa21e67efd5147d443c8321a4964f42434c0
Leonardo Oliveira <62403037+srgoogle23@users.noreply.github.com>
2021-05-24 19:39:55 -0300

    innitial

---
+++ server/app.ts
@@ -0,0 +1,50 @@
+import * as express from 'express';
+import * as morgan from 'morgan';
+import * as bodyParser from 'body-parser';
+import DataBase from './config/db';
+import CrushRoutes from './modules/crush/routes';
+import * as dotenv from 'dotenv';
+
+class App {
+  public app: express.Application;
+  private morgan: morgan.Morgan;
+  private bodyParser;
+  private database: DataBase;
+
+  constructor() {
+    dotenv.config();
+
+    this.app = express();
+    this.middleware();
+    this.routes();
+    this.database = new DataBase();
+    this.dataBaseConnection();
+  }
+
+  dataBaseConnection() {
+    this.database.createConnection();
+  }
+
+  closeDataBaseConnection(message, callback) {
+    this.database.closeConnection(message, () => callback());
+  }
+
+  middleware() {
+    this.app.use(morgan('dev'));
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+  }
+
+  routes() {
+    this.app
+      .route('/')
+      .get((req, res) => res.status(200).json({ message: 'Hello world!' }));
+    this.app.route('/api/crushs').get(CrushRoutes.getAll);
+    this.app.route('/api/crushs/:id').get(CrushRoutes.getByID);
+    this.app.route('/api/crushs').post(CrushRoutes.create);
+    this.app.route('/api/crushs/:id').put(CrushRoutes.update);
+    this.app.route('/api/crushs/:id').delete(CrushRoutes.delete);
+  }
+}
+
+export default new App();
---
+++ server/config/db.ts
@@ -0,0 +1,38 @@
+import * as mongoose from 'mongoose';
+
+class DataBase {
+  private DB_CONNECTION;
+
+  constructor() {}
+
+  createConnection() {
+    mongoose.connect(process.env.DB_URI, {
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASSWORD
+    });
+
+    this.logger(process.env.DB_URI);
+  }
+
+  logger(uri) {
+    this.DB_CONNECTION = mongoose.connection;
+    this.DB_CONNECTION.on('connected', () =>
+      console.log('Mongoose está conectado ao ' + uri),
+    );
+    this.DB_CONNECTION.on('error', error =>
+      console.error.bind(console, 'Erro na conexão: ' + error),
+    );
+    this.DB_CONNECTION.on('disconnected', () =>
+      console.log('Mongoose está desconectado do ' + uri),
+    );
+  }
+
+  closeConnection(message, callback) {
+    this.DB_CONNECTION.close(() => {
+      console.log('Mongoose foi desconectado pelo: ' + message);
+      callback();
+    });
+  }
+}
+
+export default DataBase;
